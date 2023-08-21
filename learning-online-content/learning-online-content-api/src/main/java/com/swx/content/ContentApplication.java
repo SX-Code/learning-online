d76b9f702ee44a9d720788231f6a2124c22cbd0d
SX-Code <songwuxingxox@outlook.com>
2023-08-21 21:26:40 +0800

    增加Nacos和Gateway

--- learning-online-content/learning-online-content-api/src/main/java/com/swx/content/ContentApplication.java
+++ learning-online-content/learning-online-content-api/src/main/java/com/swx/content/ContentApplication.java
@@ -2,6 +2,7 @@ package com.swx.content;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
